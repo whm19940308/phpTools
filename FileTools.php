59573c115c3c608ab49100203b265b8222bbfc07
Administrator <2723799321@qq.com>
2018-08-14 15:40:48 +0800

    加上了工具类的相关注释

--- FileTools.php
+++ FileTools.php
@@ -300,5 +300,20 @@ class FileTools{
         
     }
 
-    
+
+
+    /**
+     * @param $size
+     * @return string
+     * 文件字节转大小 K/M
+     */
+    public function format_file_size($size){
+
+        $units = array(' B', ' KB', ' MB', ' GB', ' TB');
+        for ($i = 0; $size >= 1024 && $i < 4; $i++){
+            $size /= 1024;
+        }
+        return round($size, 2).$units[$i];
+
+    }
 }

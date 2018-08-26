9ed6bbc0de564a4469b6bb4f4829c21fa1fc1c55
2723799321@qq.com <123456>
2018-08-26 21:07:48 +0800

    优化zip工具类

--- index.php
+++ index.php
@@ -1,24 +1,10 @@
 <?php
 
+// 引入两个核心文件
 require_once './_autoload.php';
 require_once './functions.php';
 
-
-
-$array = new ArrayTools();
-
-$arr = array(
-    array(
-        'id' => 1,
-        'name' => 'sb'
-    ),
-    array(
-        'id' => 2,
-        'name' => 'sb2'
-    ),
-);
-
-$data = $array->convertArrKey($arr, 'name');
-
-dump($data);
-dd($data);
+// 使用示例
+// 如调用DateTools工具类里的方法，调用示例如下
+$date_tools = new DateTools();
+var_dump($date_tools->getSomeZeroTimeStamp($str=''));
